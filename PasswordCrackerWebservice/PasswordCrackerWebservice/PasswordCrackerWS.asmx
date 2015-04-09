f148b41908506d77444b58a7041ab644f4cd475c
zxa1p1 <Ali.Ringsted@gmail.com>
2015-04-09 12:42:44 +0200

    Webservice

---
+++ PasswordCrackerWebservice/PasswordCrackerWebservice/PasswordCrackerWS.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="PasswordCrackerWS.asmx.cs" Class="PasswordCrackerWebservice.PasswordCrackerWS" %>
---
+++ PasswordCrackerWebservice/PasswordCrackerWebservice/PasswordCrackerWS.asmx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace PasswordCrackerWebservice
+{
+    /// <summary>
+    /// Summary description for PasswordCrackerWS
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class PasswordCrackerWS : System.Web.Services.WebService
+    {
+        private const string _sourceFileName = "webster-dictionary.txt";
+        private const string destinationFileName = "File-Part-{0}.txt";
+        private const int linesPerFile = 5000;
+
+
+
+        [WebMethod(Description = "Divides dictionary into chunks and sends chunks to clients")]
+        public void DoWork()
+        {
+
+            using (var sourceFile = new StreamReader(_sourceFileName))
+            {
+
+                var fileCounter = 0;
+                var destinationFile = new StreamWriter(string.Format(destinationFileName, fileCounter + 1));
+
+                try
+                {
+                    var lineCounter = 0;
+                    string line;
+                    while ((line = sourceFile.ReadLine()) != null)
+                    {
+                        if (lineCounter >= linesPerFile)
+                        {
+                            lineCounter = 0;
+                            fileCounter++;
+                            Console.WriteLine("Writing to file .. . {0}.", fileCounter);
+
+                            destinationFile.Dispose();
+                            destinationFile = new StreamWriter(string.Format(destinationFileName, fileCounter + 1));
+                        }
+                        destinationFile.WriteLine(line);
+                        lineCounter++;
+
+                    }
+                }
+                finally
+                {
+                    destinationFile.Dispose();
+                }
+
+
+            }
+            
+        }
+    }
+}
